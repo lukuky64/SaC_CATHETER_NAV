b53d6a99306ef8a9f27cb4237def47cc9a37f05c
Lucas Moore <125796088+lukuky64@users.noreply.github.com>
2023-10-12 17:10:39 +1100

    Update main.cpp

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -25,11 +25,11 @@ int main(int argc, char **argv)
     // once user input is 'yes', roslaunch RVIZ, delay for number of seconds to allow it to load
 
     // run sensor publishing node, image processing node, marker publishing node and centre prediction node
-    // the publshing node should publish the initial location but pause before publishing the rest (for the sake of the fake data)
+    // The publshing node should wait 5 seconds, publish the initial location but pause before publishing the rest (for the sake of the fake data)
 
     // User input -> Would you like to start the algorithm? (yes/no)
     // once user input is 'yes'
-    // unpause the publishing of the data and let it flow
+    // unpause the publishing of the data and let it flow (rosservice call /set_paused "data: false") but do it through c++ not terminal
 
     // LOOP
     // subscribe to the location of the catheter (EM data at the moment) and check if it has reached its end goal location
