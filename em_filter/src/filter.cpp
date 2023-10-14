7fb3968c002efe1a5c05a666ed5fd0c3717690d7
Lucas Moore <14173177@student.uts.edu.au>
2023-10-14 15:35:11 +1100

    implemneted averaging and kalman filter to filter node

--- em_filter/src/filter.cpp
+++ em_filter/src/filter.cpp
@@ -1,17 +1,91 @@
 #include "filter.h"
 
-Filter::Filter(ros::NodeHandle &nh)
+Filter::Filter(ros::NodeHandle &nh) : nh_(nh), buffer_size_(3), counter_(0)
 {
-    EM_sub_ = nh.subscribe("/em_odometry", 10, &Filter::EMCallback, this);
+    EM_sub_ = nh_.subscribe("/em_odometry", 10, &Filter::EMCallback, this);
+    EM_filtered_pub_ = nh_.advertise<geometry_msgs::PoseWithCovarianceStamped>("/em_filtered_odometry", 10, true); // latching enabled
 }
 
 Filter::~Filter()
 {
+    // Destructor
+}
+
+void Filter::kalmanFilter(double &estimate, double &error_cov, double measurement)
+{
+    // Prediction
+    error_cov += Q_;
+
+    // Update
+    K_ = error_cov / (error_cov + R_);
+    estimate += K_ * (measurement - estimate);
+    error_cov *= (1 - K_);
 }
 
 void Filter::EMCallback(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr &msg)
 {
-    // do something
+    position << msg->pose.pose.position.x, msg->pose.pose.position.y, msg->pose.pose.position.z;
+    orientation.w() = msg->pose.pose.orientation.w;
+    orientation.x() = msg->pose.pose.orientation.x;
+    orientation.y() = msg->pose.pose.orientation.y;
+    orientation.z() = msg->pose.pose.orientation.z;
+
+    // Add to buffers
+    position_buffer_.push_back(position);
+    orientation_buffer_.push_back(orientation);
+
+    // Remove oldest data if buffer is full
+    if (position_buffer_.size() > buffer_size_)
+    {
+        position_buffer_.pop_front();
+        orientation_buffer_.pop_front();
+    }
+
+    counter_++;
+    if (counter_ % buffer_size_ == 0)
+    {
+        // Average position and orientation
+        avg_position = Eigen::Vector3d::Zero();
+        avg_orientation.w() = 0;
+        avg_orientation.x() = 0;
+        avg_orientation.y() = 0;
+        avg_orientation.z() = 0;
+
+        for (const auto &pos : position_buffer_)
+        {
+            avg_position += pos;
+        }
+        avg_position /= buffer_size_;
+
+        for (const auto &ori : orientation_buffer_)
+        {
+            avg_orientation.coeffs() += ori.coeffs();
+        }
+        avg_orientation.coeffs() /= buffer_size_;
+        avg_orientation.normalize();
+
+        // Apply Kalman filter to averaged position
+        for (int i = 0; i < 3; ++i)
+        {
+            kalmanFilter(x_[i], P_[i], avg_position[i]);
+        }
+
+        filtered_pose.pose.pose.position.x = x_[0];
+        filtered_pose.pose.pose.position.y = x_[1];
+        filtered_pose.pose.pose.position.z = x_[2];
+        filtered_pose.pose.pose.orientation.w = avg_orientation.w();
+        filtered_pose.pose.pose.orientation.x = avg_orientation.x();
+        filtered_pose.pose.pose.orientation.y = avg_orientation.y();
+        filtered_pose.pose.pose.orientation.z = avg_orientation.z();
+        
+        EMPublish(filtered_pose);
+    }
+}
+
+
+void Filter::EMPublish(geometry_msgs::PoseWithCovarianceStamped msg)
+{
+    EM_filtered_pub_.publish(msg);
 }
 
 int main(int argc, char **argv)
--- em_filter/src/filter.h
+++ em_filter/src/filter.h
@@ -1,24 +1,41 @@
 #ifndef FILTER
 #define FILTER
 
-// headers
 #include <ros/ros.h>
 #include <geometry_msgs/PoseWithCovarianceStamped.h>
+#include <Eigen/Geometry>
+#include <deque>
 
-class Filter
-{
+class Filter {
 public:
-    // Constructor
     Filter(ros::NodeHandle &nh);
-
-    // Destructor
     ~Filter();
 
     void EMCallback(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr &msg);
+    void EMPublish(geometry_msgs::PoseWithCovarianceStamped msg);
 
 private:
-    ros::NodeHandle nh_;     // ROS NodeHandle for managing ROS-related operations
-    ros::Subscriber EM_sub_; // ROS Publisher for EM PoseWithCovarianceStamped messages
+    ros::NodeHandle nh_;
+    ros::Subscriber EM_sub_;
+    ros::Publisher EM_filtered_pub_;
+    std::deque<Eigen::Vector3d> position_buffer_;
+    std::deque<Eigen::Quaterniond> orientation_buffer_;
+    const size_t buffer_size_;
+    int counter_;
+    geometry_msgs::PoseWithCovarianceStamped filtered_pose;
+    Eigen::Vector3d avg_position;
+    Eigen::Quaterniond avg_orientation;
+    Eigen::Vector3d position;
+    Eigen::Quaterniond orientation;
+
+    // Kalman filter variables for position (x, y, z)
+    double x_[3] = {0, 0, 0};
+    double P_[3] = {1, 1, 1};
+    double Q_ = 0.1;
+    double R_ = 1;
+    double K_;
+
+    void kalmanFilter(double& estimate, double& error_cov, double measurement);
 };
 
 #endif // FILTER
