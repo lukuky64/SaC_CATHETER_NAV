3d728cd20d8536aff0a1d9e2421526cf94d0cfc3
Jack Gruber <grub01spx@gmail.com>
2023-10-28 15:00:08 +1100

    Commit message describing the changes

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -61,6 +61,11 @@ int main(int argc, char **argv)
     ros::Subscriber sub = nh.subscribe("camera_calibration_topic", 1000, cameraCalibrationCallback);
 
 
+    // Create a subscriber for the prediction_image topic
+    ros::Subscriber sub = nh.subscribe("prediction_image", 1000, predictionImageCallback);
+
+
+
     // User input -> Would you like to visualise the system? (yes/no)
     std::string visualize;
     std::cout << "Would you like to visualise the system? (yes/no): ";
@@ -100,20 +105,23 @@ int main(int argc, char **argv)
     std::cout << "Would you like to start the algorithm? (yes/no): ";
     std::cin >> startAlgorithm;
 
+    
     if (startAlgorithm == "yes") {
-        // Unpause the publishing of the data and let it flow (rosservice call /set_paused "data: false") but do it through C++ not terminal
-        ros::NodeHandle nh;
-        ros::ServiceClient client = nh.serviceClient<std_srvs::Empty>("/set_paused");
-        std_srvs::Empty srv;
-        if (client.call(srv))
-        {
-            ROS_INFO("Data flow unpaused");
-        }
-        else
-        {
-            ROS_ERROR("Failed to call service /set_paused");
-        }
+    // Unpause the publishing of the data and let it flow
+    ros::NodeHandle nh;
+    ros::ServiceClient client = nh.serviceClient<std_srvs::Empty>("rosservice call /set_paused");
+    std_srvs::Empty srv;
+    srv.request.data = false;  // Setting the data field to false
+
+    if (client.call(srv))
+    {
+        ROS_INFO("Data flow unpaused");
     }
+    else
+    {
+        ROS_ERROR("Failed to call service /set_paused");
+    }
+}
 
     // LOOP
     // Subscribe to the location of the catheter (EM data at the moment) and check if it has reached its end goal location
@@ -130,31 +138,7 @@ int main(int argc, char **argv)
 
     return 0;
 
-    // User input -> Would you like to visualise the system? (yes/no)
-    // once user input is 'yes', roslaunch RVIZ, delay for number of seconds to allow it to load
 
-    // run sensor publishing node, image processing node, marker publishing node
-    // The publshing node should wait 5 seconds, publish the initial location but pause before publishing the rest (for the sake of the fake data)
-    // run the camera calibration node on the first image and find scale value
-    // centre prediction node(input the scale value)
-
-    // User input -> Would you like to start the algorithm? (yes/no)
-    // once user input is 'yes'
-    // unpause the publishing of the data and let it flow (rosservice call /set_paused "data: false") but do it through c++ not terminal
-
-    // LOOP
-    // subscribe to the location of the catheter (EM data at the moment) and check if it has reached its end goal location
-    // publish estimated time and distance until from goal location
-    // LOOP
-
-    // Exit loop when end goal is reached (within some tolerance)
-
-
-
-
-    // Consider:
-    // create some interupt service routine that will pause the data publishing (and moving the robot) when the user presses space bar or something 
-    // this would be cooler if we could make some sort of GUI to do this if we have time
 
 }
 
--- main/src/main.h
+++ main/src/main.h
@@ -1,15 +1,34 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include "ros/ros.h"
+#include "std_srvs/Empty.h"
+#include "std_msgs/String.h"
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include <cstdlib>
+#include <mutex>
+#include <atomic>
+
 class Main
 {
-    public:
+public:
     Main();
     ~Main();
 
-    private:
+    // Define a callback function for the camera calibration subscriber
+    void cameraCalibrationCallback(const std_msgs::String::ConstPtr &msg);
 
+    // Define a callback function for the prediction image subscriber
+    void predictionImageCallback(const YourMessageType::ConstPtr &msg);
+
+private:
+    std::atomic<bool> pauseRequested;
+    std::mutex pauseMutex;
+    std::thread isr;
+    // Add your private variables here
 
 };
 
-#endif
+#endif // MAIN_H
